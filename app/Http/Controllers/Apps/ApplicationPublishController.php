558e362975a0c0facdafee1c8e3242f68f91a62d
qihaoran <qihaoran@wasair.com>
2019-04-25 16:04:37 +0800

    123

--- app/Http/Controllers/Apps/ApplicationPublishController.php
+++ app/Http/Controllers/Apps/ApplicationPublishController.php
@@ -46,7 +46,7 @@ class ApplicationPublishController extends Controller
         exec("sh ".$text, $result, $status);
         print_r($result);
         print_r($status);
-        //12
+        //123
 
     }
     public function quick(){
