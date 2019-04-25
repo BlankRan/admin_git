3e06e0b99666ea40c4a9a3fa766fa5325786740f
qihaoran <qihaoran@wasair.com>
2019-04-25 14:56:03 +0800

    1

--- app/Http/Controllers/Apps/ApplicationPublishController.php
+++ app/Http/Controllers/Apps/ApplicationPublishController.php
@@ -45,6 +45,7 @@ class ApplicationPublishController extends Controller
         exec("sh ".$text, $result, $status);
         print_r($result);
         print_r($status);
+        //1
 
     }
     public function quick(){
